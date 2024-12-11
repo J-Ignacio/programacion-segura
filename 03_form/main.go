8ffaf75480f8bc53e56ba0ba0209db561ee0ef3a
José Ignacio Chávez Leiva <jignaciochavezleiva@gmail.com>
2024-12-11 13:41:52 +0000

    form

--- 03_form/main.go
+++ 03_form/main.go
@@ -5,16 +5,10 @@ import (
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hola Mundo")
-}
-
 func main() {
 	// Servir archivos estáticos desde el directorio "static"
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
-
-	http.HandleFunc("/", helloHandler)
 
 	fmt.Println("Servidor iniciado en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
